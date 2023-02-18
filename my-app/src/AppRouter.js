991e58caa0b7e7f9f7502f8e7a39ac5287f875f6
krishnacheemalapati <krishkrishct@gmail.com>
2023-02-18 16:45:51 -0500

    Creating all relevant pages

---
+++ my-app/src/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/dashboard/Dashboard'
+import HomePage from './pages/home/HomePage'
+import LoginPage from './pages/Login/LoginPage'
+import About from './pages/About/About'
+
+
+class AppRouter extends React.Component{
+  render(){
+    return(
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path = "/" element = {<HomePage />} exact = {true} />
+            <Route path = "/homepage" element = {<HomePage />} exact = {true} />
+            <Route path = "/dashboard" element = {<Dashboard />} exact = {true} />
+            <Route path = "/login" element = {<LoginPage />} exact = {true} />
+            <Route path = "/about" element = {<About />} exact = {true} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default AppRouter
---
+++ my-app/src/pages/dashboard/Workout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Workout(props) {
+    // Figure out how to get stuff from the database
+    const visualizeData = () => {
+        props.setBubbleChart({ show: true, intents: res.data });
+        // Function to generate visualizations
+    }
+    const workoutName = props.workoutName;
+
+    return (
+        <div className='single-workout'>
+            <button onClick={visualizeData} className="workout button">{workoutName}</button>
+        </div>
+    )
+}
