98f37b4dee419965df3e3819fa2b203e23bc0fbf
krishnacheemalapati <krishkrishct@gmail.com>
2023-02-19 07:22:24 -0500

    visualize

---
+++ my-app/src/pages/dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import WorkoutList from './WorkoutList'
+
+export default function LoginPage() {
+    return(
+        <WorkoutList></WorkoutList>
+    );
+}
--- my-app/src/pages/dashboard/WorkoutList.js
+++ my-app/src/pages/dashboard/WorkoutList.js
@@ -1,93 +1,101 @@
-import React from 'react'
-import Workout from './Workout';
-import { TimeSeries } from "pondjs";
-import {
-    Charts,
-    ChartContainer,
-    ChartRow,
-    YAxis,
-    LineChart,
-    Baseline,
-    Resizable
-} from "react-timeseries-charts";
-export default function WorkoutList() {
-    
-    // Figure out how to get stuff from the database
-    const workouts = []
-    const series = new TimeSeries({
-        name: "Heart Rate",
-        columns: ["time", "value"],
-        workouts
-    });
+import React from "react";
+import Workout from "./Workout";
+import { Component } from "react";
+import LineChart from "react-linechart";
+import "../../../node_modules/react-linechart/dist/styles.css";
+// import { TimeSeries } from "pondjs";
+// import {
+//   Charts,
+//   ChartContainer,
+//   ChartRow,
+//   YAxis,
+//   LineChart,
+//   Baseline,
+// } from "react-timeseries-charts";
+/* App.js */
+// import { Component } from "react";
+import CanvasJSReact from "./../../assets/canvasjs-3.7.5/canvasjs.react";
+// var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+export default class WorkoutList extends Component {
+  render() {
+    const data = [
+      {
+        color: "steelblue",
+        points: [
+          { x: 1, y: 2 },
+          { x: 3, y: 5 },
+          { x: 7, y: -3 },
+        ],
+      },
+    ];
+    const workouts = ["Workout 1", "Workout 2", "Workout 3"];
 
-    const style = {
-        value: {
-            stroke: "#a02c2c",
-            opacity: 0.2
-        }
-    };
-
-    const baselineStyle = {
-        line: {
-            stroke: "steelblue",
-            strokeWidth: 1,
-            opacity: 0.4,
-            strokeDasharray: "none"
-        },
-        label: {
-            fill: "steelblue"
-        }
-    };
-    
-    const baselineStyleLite = {
-        line: {
-            stroke: "steelblue",
-            strokeWidth: 1,
-            opacity: 0.5
-        },
-        label: {
-            fill: "steelblue"
-        }
-    };
-
-    return(
-        <div className="dashboard">
+    return (
+      <div className="dashboard">
         <div className="workout-management">
-            <div className="newWorkout">
+          <div className="newWorkout">
             <h2 className="filesHeader" id="tm">
-                Past Workouts
+              Past Workouts
             </h2>
+          </div>
+          <div className="oldWorkouts">
+            <h2 className="filesHeader">Recorded Workouts</h2>
+            <div className="workoutContainer">
+              {workouts === undefined ? (
+                <p className="inactive">No workouts have been recorded</p>
+              ) : (
+                workouts.map((workout) => (
+                  <Workout key={workout} name={workout} />
+                ))
+              )}
             </div>
-                <div className="oldWorkouts">
-                    <h2 className="filesHeader">Recorded Workouts</h2>
-                    <div className="workoutContainer">
-                    {workouts === undefined ? (
-                        <p className="inactive">No workouts have been recorded</p>
-                    ) : (
-                        workouts.map((workout) => (
-                        <Workout key={workout} name={workout} />
-                        ))
-                    )}
-                </div>
-            </div>
+          </div>
         </div>
-        <ChartContainer timeRange={series.range()} format="%b '%y">
-        <ChartRow height="150">
-            <YAxis
-                id="price"
-                label="Price ($)"
-                min={series.min()} max={series.max()}
-                width="60" format="$,.2f"/>
-            <Charts>
-                <LineChart axis="price" series={series} style={style}/>
-                <Baseline axis="price" style={baselineStyleLite} value={series.max()} label="Max" position="right"/>
-                <Baseline axis="price" style={baselineStyleLite} value={series.min()} label="Min" position="right"/>
-                <Baseline axis="price" style={baselineStyleLite} value={series.avg() - series.stdev()}/>
-                <Baseline axis="price" style={baselineStyleLite} value={series.avg() + series.stdev()}/>
-                <Baseline axis="price" style={baselineStyle} value={series.avg()} label="Avg"/>
-            </Charts>
-        </ChartRow>
-    </ChartContainer>
-    </div>
+        <div>
+            <div className="App">
+                <p>My First LineChart</p>
+                <LineChart 
+                    width={600}
+                    height={400}
+                    data={data}
+                    xLabel="Time"
+                    yLabel="BPM"
+                />
+            </div>				
+        </div>
+      </div>
     );
+  }
 }
+// module.exports = WorkoutList;
+// export default WorkoutList;
+// import React, { Component } from 'react';
+// import LineChart from 'react-linechart';
+// import '../../../node_modules/react-linechart/dist/styles.css';
+
+// export default class WorkoutList extends Component {
+// 	render() {
+// const data = [
+// 	{
+// 		color: "steelblue",
+// 		points: [
+//             {x: 1, y: 2},
+//             {x: 3, y: 5},
+//             {x: 7, y: -3}]
+// 	}
+// ];
+// 		return (
+// 			<div>
+// 				<div className="App">
+// 					<p>My First LineChart</p>
+// 					<LineChart
+// 						width={600}
+// 						height={400}
+// 						data={data}
+// 					/>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
