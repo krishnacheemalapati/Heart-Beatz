992dc2a34314aff1020ec9394a7d6bcee01054dd
chait27 <105755252+chait27@users.noreply.github.com>
2023-02-19 03:01:27 -0500

    ive lost my mind

--- heart_rate_detector/talktopy.py
+++ heart_rate_detector/talktopy.py
@@ -1,24 +1,15 @@
+import serial
 from serial import Serial, SerialException
 import mysql.connector
 import numpy as np
 from datetime import datetime
 
-start=False
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="user",
-  password="pass",
-  database="workouts"
-)
-
-
+start=True
 
 try:
-    SObj = Serial('COM7',9600)
+    SObj = Serial('COM7', 9600, timeout=1)
     
-    
-except SerialException as var : 
+except SerialException as var: 
     print('An Exception Occured')
     print('Exception Details-> ', var)
     
@@ -29,25 +20,41 @@ heartbeats = [0, 0, 0, 0, 0]
 beatcount = 0
 
 line = SObj.readline()
-if line == "ALIVE": start==True
+
+#if line == "ALIVE":
+    #start=True
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="user",
+  password="pass",
+  database="workouts"
+)
 
 while(start):
     try:
         ReceivedString = SObj.readline()
-        if(ReceivedString != 'DEAD'):
+        temp = ReceivedString.decode('UTF-8')
+        if(temp != 'DEAD'):
+            temp = temp[:-2]
             now = datetime.now()
-            heatbeats[beatcount] = int(ReceivedString)
+            
+            heartbeats[beatcount] = int(float(temp))
+            beatcount+=1
             if (beatcount >= 5):
                 beatcount = 0
             
-            mean = np.mean(heatbeats)
-            beattime = str(now.hour) +":" + str(now.minute) + ":" + str(now.second)
-            pop_client = "INSERT INTO tbl_heartbeat VALUES(1, 1, "+beattime+', '+ str(int(mean)) + ");"            
+            mean = np.mean(heartbeats)
+            bt = str(now.hour) +":" + str(now.minute) + ":" + str(now.second)
+            x = str(int(mean))
             
             mycursor = mydb.cursor()
+            
+            pop_client = 'INSERT INTO tbl_heartbeat(userid, wid, beattime, bpm) VALUES(1, 1, \'' + bt + '\', \'' +  x + '\');'            
+            
             mycursor.execute(pop_client)
             mydb.commit()
-
+            #print(pop_client)
         else:
             print('User Stoped')
             start=False
