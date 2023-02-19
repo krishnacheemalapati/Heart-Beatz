42241dc15c8b4253638232cf43d4fb21a91417b4
swaner2 <j.chen1904@gmail.com>
2023-02-19 00:08:48 -0500

    alive and dead

--- heart_rate_detector/heart_rate_detector.ino
+++ heart_rate_detector/heart_rate_detector.ino
@@ -17,42 +17,53 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
 }
 
+int button_count = 0;
+
 void loop() {
   bool button = digitalRead(BUTTON_PIN);
-  if (button == true){
+  if (button == true) {
+    if (button_count == 0){
+      Serial.println("ALIVE");
+    }
+    button_count++;
+  }
+
+  if (button_count == 2){
     Serial.print("DEAD");
     exit(0);
   }
 
-  reading = analogRead(0);
+  if (button_count == 1){
+    reading = analogRead(0);
 
-  // Heart beat leading edge detected.
-  if (reading > threshold && ignoreReading == false) {
-    if (firstPulseDetected == false) {
-      firstPulseTime = millis();
-      firstPulseDetected = true;
-    } else {
-      secondPulseTime = millis();
-      pulseInterval = secondPulseTime - firstPulseTime;
-      firstPulseTime = secondPulseTime;
+    // Heart beat leading edge detected.
+    if (reading > threshold && ignoreReading == false) {
+      if (firstPulseDetected == false) {
+        firstPulseTime = millis();
+        firstPulseDetected = true;
+      } else {
+        secondPulseTime = millis();
+        pulseInterval = secondPulseTime - firstPulseTime;
+        firstPulseTime = secondPulseTime;
+      }
+      ignoreReading = true;
     }
-    ignoreReading = true;
-  }
 
-  // Heart beat trailing edge detected.
-  if (reading < threshold) {
-    ignoreReading = false;
-  }
-  int bpmTemp = (1.0 / pulseInterval) * 60.0 * 1000;
+    // Heart beat trailing edge detected.
+    if (reading < threshold) {
+      ignoreReading = false;
+    }
+    int bpmTemp = (1.0 / pulseInterval) * 60.0 * 1000;
 
-  bpmArray[1] = bpmArray[0];
-  bpmArray[0] = bpmTemp;
+    bpmArray[1] = bpmArray[0];
+    bpmArray[0] = bpmTemp;
 
-  BPM = (bpmArray[1] + bpmArray[0]) / 2.0;
+    BPM = (bpmArray[1] + bpmArray[0]) / 2.0;
 
 
-  Serial.println(BPM);
-  Serial.flush();
+    Serial.println(BPM);
+    Serial.flush();
 
-  delay(100);
+    }
+    delay(100);
 }
